2242f8c4e9cc334e41320f306afca217acad2c40
craftor-3622 <wnsjh9110@gmail.com>
2025-10-21 12:37:16 +0900

    fix: clang-format 설정 변경

--- Qt/main.cpp
+++ Qt/main.cpp
@@ -1,14 +1,15 @@
-#include "mainwindow.h"
+#include <QTranslator>
 
 #include <QApplication>
 #include <QLocale>
-#include <QTranslator>
+
+#include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QTranslator translator;
+    QTranslator       translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
     for (const QString &locale : uiLanguages) {
         const QString baseName = "Qt_" + QLocale(locale).name();
--- Qt/mainwindow.h
+++ Qt/mainwindow.h
@@ -4,8 +4,9 @@
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
+namespace Ui
+{
+    class MainWindow;
 }
 QT_END_NAMESPACE
 
@@ -13,11 +14,11 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
-public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+      public:
+        MainWindow(QWidget *parent = nullptr);
+        ~MainWindow();
 
-private:
-    Ui::MainWindow *ui;
+      private:
+        Ui::MainWindow *ui;
 };
-#endif // MAINWINDOW_H
+#endif  // MAINWINDOW_H
