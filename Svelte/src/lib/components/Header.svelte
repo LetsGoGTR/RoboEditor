df40fe40bb67c847240acc595ad24240c5bb8654
craftor-3622 <wnsjh9110@gmail.com>
2025-11-04 10:14:43 +0900

    feat: 헤더 & Menu bar UI 구현 (S13P31S205-70 wip)

---
+++ Svelte/src/lib/components/Header.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+</script>
+
+<header class="header">
+	<span class="title">RoboEditor</span>
+	<span class="file-name">파일 이름</span>
+</header>
+
+<style>
+	.header {
+		margin: 0;
+		padding: 8px 12px;
+
+    align-items: center;
+	}
+
+	.title {
+		margin-right: 30px;
+
+		font-size: 24px;
+		font-weight: 700;
+	}
+
+	.file-name {
+		font-size: 20px;
+		font-weight: 400;
+	}
+</style>
---
+++ Svelte/src/lib/components/MenuBar.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+</script>
+
+<div class="full-width" role="menubar">
+	<ul class="menu">
+		<li>
+			<span>파일</span>
+			<ul class="dropdown">
+				<li><span>새로 만들기</span></li>
+				<li><span>불러오기</span></li>
+				<li><hr /></li>
+				<li><span>시뮬레이션</span></li>
+			</ul>
+		</li>
+		<li><span>도구</span></li>
+		<li><span>제어기</span></li>
+		<li><span>설정</span></li>
+	</ul>
+</div>
+
+<style>
+	.full-width {
+		display: block; /* 인라인 요소라면 block으로 변경 */
+		width: 100%; /* 부모 너비의 100% 차지 */
+		background: #4e83db;
+	}
+
+	.menu {
+		list-style: none;
+		margin: 0;
+		padding: 0;
+		display: flex;
+	}
+
+	.menu > li {
+		position: relative;
+	}
+
+	ul > li > span {
+		display: block;
+		padding: 10px 20px;
+		color: #fff;
+		text-decoration: none;
+	}
+
+	ul > li:hover > span {
+		color: #000;
+		background: #fff;
+	}
+
+	/* 드롭다운 숨김 */
+	.dropdown {
+		display: none;
+		position: absolute;
+		top: 100%;
+		left: 0;
+
+		min-width: 200px;
+
+		list-style: none;
+		margin: 0;
+		padding: 0;
+		background: #4675c7;
+	}
+
+	/* 마우스 오버 시 표시 */
+	.menu > li:hover .dropdown {
+		display: block;
+	}
+</style>
