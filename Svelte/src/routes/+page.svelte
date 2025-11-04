658381e77e0b86dfa8f030afdad21d3253c2b9c8
craftor-3622 <wnsjh9110@gmail.com>
2025-11-04 10:14:43 +0900

    feat: 헤더 & Menu bar UI 구현 (S13P31S205-70 wip)

--- Svelte/src/routes/+page.svelte
+++ Svelte/src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Header from '../lib/components/Header.svelte';
+	import MenuBar from '../lib/components/MenuBar.svelte';
+</script>
+
+<Header />
+<MenuBar />
+
+<style>
+</style>
