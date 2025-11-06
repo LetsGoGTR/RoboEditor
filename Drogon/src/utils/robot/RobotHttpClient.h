e94ee0212e2d9c7261c156dc48af6bf9db3d9674
gonudayo <kgw010119@gmail.com>
2025-11-06 16:42:36 +0900

    fix: path error

--- Drogon/src/utils/robot/RobotHttpClient.h
+++ Drogon/src/utils/robot/RobotHttpClient.h
@@ -20,11 +20,21 @@ namespace utils
         downloadWorkspace(const std::string                                            &ip,
                           std::function<void(const std::string &, const std::string &)> callback);
 
+        static void
+        archiveWorkspace(const std::string                                            &ip,
+                         std::function<void(const std::string &, const std::string &)> callback);
+
+        static void extractArchive(const std::string                             &ip,
+                                   const std::string                             &remoteArchivePath,
+                                   std::function<void(bool, const std::string &)> callback);
+
       private:
         // Robot API endpoints
-        static constexpr const char *RUNNING_ENDPOINT = "/api/v1/robot/running";
-        static constexpr const char *IMPORT_ENDPOINT  = "/api/v1/robot/import";
-        static constexpr const char *EXPORT_ENDPOINT  = "/api/v1/robot/export";
+        static constexpr const char *RUNNING_ENDPOINT = "/api/robot/running";
+        static constexpr const char *IMPORT_ENDPOINT  = "/api/robot/import";
+        static constexpr const char *EXPORT_ENDPOINT  = "/api/robot/export";
+        static constexpr const char *ARCHIVE_ENDPOINT = "/api/robot/archive";
+        static constexpr const char *EXTRACT_ENDPOINT = "/api/robot/extract";
     };
 
 }  // namespace utils
